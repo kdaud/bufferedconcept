b3e430bec6c6347c4f005b3fc9fcfa3f5dcc89e6
kdaud <dkakumirizii@gmail.com>
2020-01-20 13:51:12 +0300

    my codes

--- org/javacodes/Tester.java
+++ org/javacodes/Tester.java
@@ -1,10 +1,17 @@
 package javacodes;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Tester {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello Google");
-		
+		Map<String, String> student = new HashMap<>();
+		student.put("Gama", "BSS");
+		student.put("Edward", "EDS");
+		String course = student.get("Daud");
+		System.out.println(course);
 	}
 	
 }
