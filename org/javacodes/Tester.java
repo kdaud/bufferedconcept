5d921a4cdc38096c42ffb6eeb9a4124d0245900c
kdaud <dkakumirizii@gmail.com>
2020-01-20 14:20:06 +0300

    codes

--- org/javacodes/Tester.java
+++ org/javacodes/Tester.java
@@ -1,6 +1,7 @@
 package javacodes;
 
 import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.Map;
 
 public class Tester {
@@ -11,9 +12,23 @@ public class Tester {
 		student.put("Gama", "BSS");
 		student.put("Edward", "EDS");
 		student.put("Gama", "BIT");
+		student.put("Ruth", "BIT");
 		String course = student.get("Gama");
 		System.out.println(course);
-		//System.out.println(student.getClass().desiredAssertionStatus());
+		
+		Map<String, String > lecturer= new Hashtable<>();
+		boolean capacity = lecturer.isEmpty();
+		System.out.println(capacity);
+		lecturer.put("Kinene", "Java");
+		int capacitys = lecturer.size();
+		System.out.println(capacitys);
+		lecturer.put("Joseph", "Php");
+		lecturer.put("Kinene", "C#");
+		
+		System.out.println(capacity);
+		
+		String myteacher = lecturer.getOrDefault("Kinene", "Python");
+		System.out.println(myteacher);
 	}
 	
 }
