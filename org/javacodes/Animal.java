03368c655980465e77ee6f2249dd98f6e65e79c3
kdaud <dkakumirizii@gmail.com>
2020-01-20 13:44:41 +0300

    codes

---
+++ org/javacodes/Animal.java
@@ -0,0 +1,15 @@
+package javacodes;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Animal {
+	
+	private Map<String, String> phonebook;
+	
+	public String coding() {
+		phonebook = new HashMap<>(5);
+		phonebook.put("Kicol", "256772509867");
+		return coding();
+	}
+}
--- org/javacodes/Tester.java
+++ org/javacodes/Tester.java
@@ -1,10 +1,10 @@
 package javacodes;
 
-
 public class Tester {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello Google");
+		
 	}
 	
 }
