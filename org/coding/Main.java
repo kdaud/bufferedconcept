189c25f6a19ff043f9f5567d1d7206c6a7a91b0f
kdaud <dkakumirizii@gmail.com>
2020-01-20 12:43:43 +0300

    codes

---
+++ org/coding/Main.java
@@ -0,0 +1,13 @@
+package coding;
+
+
+public class Main {
+	
+	public static void main(String[] args) {
+		new Benz();
+		Vehicles hg = new Wish();
+		Master obj = new Master();
+		obj.myCode(hg);
+	}
+	
+}
